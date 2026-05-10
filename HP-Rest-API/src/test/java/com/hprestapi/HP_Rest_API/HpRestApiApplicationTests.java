90a0d1de533d5109405b4c3b89bf56c9588b7b2a
sajal <sajal.mainhuna@gmail.com>
2026-05-10 19:23:17 +0530

    New Project

---
+++ HP-Rest-API/src/test/java/com/hprestapi/HP_Rest_API/HpRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hprestapi.HP_Rest_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
